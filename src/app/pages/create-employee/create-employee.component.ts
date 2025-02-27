635501b0e94356b66868fdfd096a84ba4ca5006e
Gopesh Sharma <gopesh.sharma@gmail.com>
2025-02-26 21:01:10 -0500

    completed add employee form

--- src/app/pages/create-employee/create-employee.component.ts
+++ src/app/pages/create-employee/create-employee.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-create-employee',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, NgIf],
   templateUrl: './create-employee.component.html',
-  styleUrl: './create-employee.component.css'
+  styleUrl: './create-employee.component.css',
 })
 export class CreateEmployeeComponent {
-
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('form data valid', this.loginForm.value);
+    }
+  }
 }
